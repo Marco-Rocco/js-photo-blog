dab2bcb58a79a19df163b427cf3bd931363a3e55
Marco-Rocco <marcorocco28@icloud.com>
2025-03-14 15:30:53 +0100

    feat: crete first js elements and fix spacing between cards

---
+++ script.js
@@ -0,0 +1,13 @@
+//collego api
+const photoElements = 'https://lanciweb.github.io/demo/api/pictures/'
+
+//collego elemento hmtl
+let rowContainer = document.getElementById('row')
+console.log (rowContainer)
+
+//invoca funzione
+
+//crea funzione (nel caso ci fosse bisogno di un bottone in futuro)
+for (let i = 1; i <= 6; i++) {
+    
+}
