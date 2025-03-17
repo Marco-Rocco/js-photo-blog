40c6c3d133f9042fee1d61dd5755427f6976ae87
Marco-Rocco <marcorocco28@icloud.com>
2025-03-17 13:06:34 +0100

    feat: show only selected image in the overlay

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ console.log (rowContainer)
                 // a result CONCATENIAMO l'html
                 result += `<div class="col lg-4 md-6 sm-12">
                 
-                <div class="card" onclick="on()">
+                <div class="card">
                     <img src="${url}" class="card-img-top" alt="${title}">
                     <div class="card-body">
                       <p class="card-text">${date}</p>
@@ -44,8 +44,24 @@ console.log (rowContainer)
               rowContainer.innerHTML = result
             });
             
-            
-            
+
+
+            //collego la card
+            const cards = document.querySelectorAll('.card');
+            console.log(cards)
+            // creo un ciclo for per scorrere l'array, poi un addeventlistener che mi richiama la funzione
+            for (let i = 0; i < cards.length; i++) {
+                let card = cards[i];  
+                card.addEventListener('click', on)             
+            }
+
+            //collego il bottone
+            const overlayButton = document.getElementById('overlay-button')
+            console.log(overlayButton)
+            //creo addeventlistener che richiama la funzione off
+            overlayButton.addEventListener('click', off)
+
+
             // console.log(dataArray)
         })
         .catch ((error => {
@@ -55,16 +71,36 @@ console.log (rowContainer)
 
 
 
-        // overlay
+// overlay
 
-        //collego l'immagine dell'overlay
-        const overlayImg = document.getElementById('overlay-img')
-        console.log(overlayImg)
 
-        function on() {
-            document.getElementById("overlay").style.display = "block";
-          }
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+    }
+    
+function off() {
+    document.getElementById("overlay").style.display = "none";
+    } 
           
-          function off() {
-            document.getElementById("overlay").style.display = "none";
-          }
+          
+          
+        
+// mostra immagine selezionata  
+
+
+
+// //collego l'immagine dell'overlay
+// const overlayImg = document.getElementById('overlay-img')
+// console.log(overlayImg)
+
+
+
+// card.addEventListener('click', function(){
+//     overlayImg = this(overlayImg);
+
+//     overlayImg.src = `${url}`
+//     overlayImg.alt = `${title}`
+// })
+
+
